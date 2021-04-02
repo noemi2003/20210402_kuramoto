65fcac4dae8893b49c9dad49c396a47838ce9cad
noemi <kuramotonoemi@kuramotonowarainoMacBook-Air.local>
2021-04-02 19:47:01 +0900

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="app">
+  <div class="accordion" @click="show">
+    <p>アコーディオン</p>
+  </div>
+  <div class="accordionBody" v-if="open">
+    <p>アコーディオンの中身が入ります</p>
+  </div>
+</div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  data(){
+    return{
+      open: false,
+    }
+  },
+  methods:{
+    show(){
+      this.open = !this.open
+    }
+  }
+}
+</script>
+
+<style scoped>
+p{
+  margin: 0;
+  padding: 10px;
+}
+.app {
+  border: 1px solid black;
+  width: 30vw;
+}
+.accordion {
+  background-color: rgb(218, 218, 218);
+  height: 100%;
+}
+
+.accordionBody{
+
+}
+</style>
